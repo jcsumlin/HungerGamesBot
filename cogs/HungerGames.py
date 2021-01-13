5f3d8ed75a34fa54ed31bd364f71cb7485cd8685
chat.sumlin <chat.sumlin@payroc.com>
2021-01-13 11:07:09 -0500

    Initial commit

---
+++ cogs/HungerGames.py
@@ -0,0 +1,23 @@
+import os, sys, discord, platform, random, aiohttp, json
+from discord.ext import commands
+
+if not os.path.isfile("config.py"):
+    sys.exit("'config.py' not found! Please add it and try again.")
+else:
+    import config
+
+
+class HungerGames(commands.Cog, name="HungerGames"):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.group(name="tribute")
+    async def tribute(self, context):
+        pass
+
+    @tribute.command(name="add")
+    async def __add(self, context):
+        pass
+
+def setup(bot):
+    bot.add_cog(HungerGames(bot))
