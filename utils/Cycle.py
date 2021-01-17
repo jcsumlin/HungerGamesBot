3e37b26a61c1a1307fcb2bbefc77bcf3b5ff6a95
AyJayTee <aidanjthomas97@gmail.com>
2021-01-17 13:49:38 +0000

    Updated Cycle.py to incorporate day/night time constraints

--- utils/Cycle.py
+++ utils/Cycle.py
@@ -2,10 +2,14 @@ import random
 
 
 class Cycle:
-    def __init__(self, tributes: list):
+    def __init__(self, tributes: list, daytime: bool):
         self.tributes = tributes
         self.number_of_tributes = len(tributes)
         self.unassigned_tributes = self.number_of_tributes
+        if daytime:
+            self.time = "Day"
+        else:
+            self.time = "Night"
 
         """The weights dictionary defines the probability of any given event
         including x number of tributes"""
@@ -44,6 +48,7 @@ class Cycle:
         return list_of_events
 
     def select_tributes(self, list_of_events: list):
+        """Sorts tributes into a list of groups of tributes involved in each event"""
         sorted_tributes = []
         for event in list_of_events:
             tributes_included = []
@@ -52,3 +57,26 @@ class Cycle:
                 self.tributes.pop(self.tributes.index(tribute))
                 tributes_included.append(tribute)
             tributes_included.append(sorted_tributes)
+        return sorted_tributes
+
+    def get_events(self):
+        """Returns the days events as a list of event objects"""
+        events = []
+        list_of_events = self.find_number_of_tributes_in_events()
+        tribute_groups = self.select_tributes(list_of_events)
+        for tribute_group in tribute_groups:
+            n = len(tribute_group)
+            if self.time == "Day":
+                event = self.__get_daytime_event__()
+            else:
+                event = self.__get_nighttime_event()
+            events.append(event)
+        return events
+
+    def __get_daytime_event__(self):
+        """Returns a random daytime event that matches the constraints"""
+        pass
+
+    def __get_nighttime_event(self):
+        """Returns a random nighttime event that matches the constraints"""
+        pass
